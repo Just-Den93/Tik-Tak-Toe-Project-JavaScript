9074cf8427203783da006708e5f9c4bcae958dee
Denis <andreev.denis0693@gmail.com>
2022-09-12 17:35:12 +0300

    TIK TAK TOE 1. Functions isPlayerOneWin, isPlayerSecond, addActiveClass, addNegativeClass are deduced to util 2. Functions isPlayerOneWin, isPlayerSecond simplified with every(); 3. Added handlePlayerClick function instead of the previous two;

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ for(let i = 0; i < 9; i++) {
 
 let game = document.getElementById('game');
 let blocks = document.querySelectorAll('.block');
-let res = document.querySelector('.result');
+let stat = document.querySelector('.status-text');
 let btnGame = document.querySelector('.new-game');
-let currentPlayer = document.getElementById('cur-plr');
 
 let step = false;
 let player = '';
@@ -15,39 +14,50 @@ let score = {
     'o': 0,
     'd': 0,
 };
-count = 0;
+let count = 0;
 let playerOne = 'x';
 let playerSecond = '0';
 
-function stepPlayerOne(target) {
-    target.innerHTML = playerOne;
-    target.classList.add('playerOne');
-    count++;
-};
-function stepPlayerSecond(target) {
-    target.innerHTML = playerSecond;
-    target.classList.add('playerSecond');
-    count++;
-};
-function init(e) {
-     if(!step) stepPlayerOne(e.target);
-     else stepPlayerSecond(e.target);
-     step = !step;
-    //  score[player] += 1;
-     player = player == '0' ? 'x' : '0';
-     currentPlayer.innerHTML = player.toUpperCase();
-     checkWin();
+const isPlayerOneWin = (blocks, comb, i) => [0, 1, 2].every(index => blocks[comb[i][index]].classList.contains(playerOne));
+const isPlayerSecond = (blocks, comb, i) => [0, 1, 2].every(index => blocks[comb[i][index]].classList.contains(playerSecond));
 
+const addActiveClass = (blocks, comb, i) => {
+    blocks[comb[i][0]].classList.add('active');
+    blocks[comb[i][1]].classList.add('active');
+    blocks[comb[i][2]].classList.add('active');
+};
+
+const addNegativeClass = (blocks, comb, i) => {
+    blocks[comb[i][0]].classList.add('negative');
+    blocks[comb[i][1]].classList.add('negative');
+    blocks[comb[i][2]].classList.add('negative');
+};
+
+const handlePlayerClick = (target, player) => {
+    if(target.innerHTML !== playerOne && target.innerHTML !== playerSecond) {
+        target.innerHTML = player;
+        target.classList.add(player);
+        count++;
+        player = player == playerSecond ? playerOne : playerSecond;
+        stat.textContent = `${player} Turn`;
+    }
+};
+
+function init(e) {
+     if(!step) handlePlayerClick(e.target, playerOne); 
+     else handlePlayerClick(e.target, playerSecond);
+     step = !step;
+     checkWin();
 };
 
 function newGame() {
     step = false;
     count= 0;
-    res.innerText = '';
+    btnGame.style.display = 'none';
     blocks.forEach(item => {
         item.innerHTML = '';
-        currentPlayer.innerHTML = '';
-        item.classList.remove('playerOne', 'playerSecond', 'active', 'negative');
+        stat.innerHTML = `X Turn`;
+        item.classList.remove(playerOne, playerSecond, 'active', 'negative');
         game.addEventListener('click', init);
         updateScore()
     })
@@ -57,7 +67,7 @@ function updateScore() {
     document.getElementById('sX').innerHTML = score.x;
     document.getElementById('sO').innerHTML = score.o;
     document.getElementById('sD').innerHTML = score.d;
-}
+};
 
 function checkWin() {
     let comb = [
@@ -73,40 +83,28 @@ function checkWin() {
     
     for(let i = 0; i < comb.length; i++) {
 
-        if (blocks[comb[i][0]].classList.contains('playerOne') &&
-            blocks[comb[i][1]].classList.contains('playerOne') &&
-            blocks[comb[i][2]].classList.contains('playerOne')) {
-                setTimeout(() => {
-                    blocks[comb[i][0]].classList.add('active');
-                    blocks[comb[i][1]].classList.add('active');
-                    blocks[comb[i][2]].classList.add('active');
-                    res.innerText = 'Cross Win';
-                }, 100);
-                game.removeEventListener('click', init)
-                score.x += 1
+        if (isPlayerOneWin(blocks, comb, i)) {
+            addActiveClass(blocks, comb, i);
+            stat.innerText = `X Wins!`;
+            game.removeEventListener('click', init)
+            score.x += 1
+            btnGame.style.display = "block"
         }
 
-        else if (blocks[comb[i][0]].classList.contains('playerSecond') &&
-        blocks[comb[i][1]].classList.contains('playerSecond') &&
-        blocks[comb[i][2]].classList.contains('playerSecond')) {
-            setTimeout(() => {
-                blocks[comb[i][0]].classList.add('active');
-                blocks[comb[i][1]].classList.add('active');
-                blocks[comb[i][2]].classList.add('active');
-                res.innerText = 'Zero Win';
-            }, 100);
+        else if (isPlayerSecond(blocks, comb, i)) {
+            addActiveClass(blocks, comb, i);
+            stat.innerText = `0 Wins!`;
             game.removeEventListener('click', init)
             score.o += 1
+            btnGame.style.display = "block"
         }
 
         else if (count == 9 && i === 7) {
-            blocks[comb[i][0]].classList.add('negative');
-            blocks[comb[i][1]].classList.add('negative');
-            blocks[comb[i][2]].classList.add('negative');
-            res.innerText = 'Draw, try again';
+            addNegativeClass(blocks, comb, i);
+            stat.innerText = `It's a Tie`;
             game.removeEventListener('click', init);
-            console.log(score.d);
             score.d += 1;
+            btnGame.style.display = "block"
         }
     }
 };
